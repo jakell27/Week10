3cf7b37bde4ab23dd3a43617d987e9dada3b48c9
jakell27 <109448880+jakell27@users.noreply.github.com>
2022-09-10 18:20:29 -0600

    Add files via upload

---
+++ mysql-java/src/main/java/projects/entity/Project.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package projects.entity;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Promineo
+ *
+ */
+public class Project {
+  private Integer projectId;
+  private String projectName;
+  private Integer estimatedHours;
+  private Integer actualHours;
+  private Integer difficulty;
+  private String notes;
+
+  private List<Material> materials = new LinkedList<>();
+  private List<Step> steps = new LinkedList<>();
+  private List<Category> categories = new LinkedList<>();
+
+  public Integer getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(Integer projectId) {
+    this.projectId = projectId;
+  }
+
+  public String getProjectName() {
+    return projectName;
+  }
+
+  public void setProjectName(String projectName) {
+    this.projectName = projectName;
+  }
+
+  public Integer getEstimatedHours() {
+    return estimatedHours;
+  }
+
+  public void setEstimatedHours(Integer estimatedHours) {
+    this.estimatedHours = estimatedHours;
+  }
+
+  public Integer getActualHours() {
+    return actualHours;
+  }
+
+  public void setActualHours(Integer actualHours) {
+    this.actualHours = actualHours;
+  }
+
+  public Integer getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(Integer difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+
+  public List<Material> getMaterials() {
+    return materials;
+  }
+
+  public List<Step> getSteps() {
+    return steps;
+  }
+
+  public List<Category> getCategories() {
+    return categories;
+  }
+
+  @Override
+  public String toString() {
+    String result = "";
+    
+    result += "\n   ID=" + projectId;
+    result += "\n   name=" + projectName;
+    result += "\n   estimatedHours=" + estimatedHours;
+    result += "\n   actualHours=" + actualHours;
+    result += "\n   difficulty=" + difficulty;
+    result += "\n   notes=" + notes;
+    
+    result += "\n   Materials:";
+    
+    for(Material material : materials) {
+      result += "\n      " + material;
+    }
+    
+    result += "\n   Steps:";
+    
+    for(Step step : steps) {
+      result += "\n      " + step;
+    }
+    
+    result += "\n   Categories:";
+    
+    for(Category category : categories) {
+      result += "\n      " + category;
+    }
+    
+    return result;
+  }
+}
